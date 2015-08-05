d96e998f2b99d44ce4f6b33be3a1135e3af50c6a
Scott McCaulay <scottmccaulay@gmail.com>
2015-08-04 20:44:17 -0400

    Revert "added method to retrieve RO data from PDT"
    This reverts commit 485725b82837195d94daa688b86b6bbba40fbb86.

--- va-workflow/src/main/java/org/sead/workflow/activity/impl/UpdatePdtActivity.java
+++ va-workflow/src/main/java/org/sead/workflow/activity/impl/UpdatePdtActivity.java
@@ -1,11 +1,7 @@
 package org.sead.workflow.activity.impl;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.sead.workflow.activity.AbstractWorkflowActivity;
 import org.sead.workflow.activity.SeadWorkflowActivity;
 import org.sead.workflow.config.SeadWorkflowConfig;
@@ -15,7 +11,6 @@ import org.sead.workflow.util.Constants;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-
 import org.seadva.services.statusTracker.SeadStatusTracker;
 import org.seadva.services.statusTracker.enums.SeadStatus;
 
@@ -40,42 +35,23 @@ public class UpdatePdtActivity extends AbstractWorkflowActivity {
                 break;
             }
         }
-        String pdtSystemUrl = activityParams.get("pdtSystemUrl");  
 
         SeadStatusTracker.addStatus(context.getProperty(Constants.RO_ID), SeadStatus.PDTStatus.START.getValue());
 
-        String ros = null;
-        JSONObject ro = null;
-        JSONObject roObject = new JSONObject();
-		try {
-			ro = new JSONObject(context.getProperty(Constants.JSON_RO));
-			
-            roObject.put("@context", "http://schema.org/");
-            roObject.put("@type", "DataDownload");
-            if (ro.has("Identifier"))
-            	roObject.put("Identifier", ro.get("Identifier"));
-            if (ro.has("Uploaded By"))
-            	roObject.put("Uploaded By", ro.get("Uploaded By"));
-            if (ro.has("contentUrl"))
-            	roObject.put("contentUrl", ro.get("contentUrl"));
-            if (ro.has("Title"))
-            	roObject.put("Title", ro.get("Title"));
-            if (ro.has("Creator"))
-            	roObject.put("Creator", ro.get("Creator"));
-            if (ro.has("Published In"))
-            	roObject.put("Published In", ro.get("Published In"));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        String ro = context.getProperty(Constants.JSON_RO);
+        String pdtSystemUrl = activityParams.get("pdtSystemUrl");  
         
-            ros = roObject.toString(); 		    
+        // for testing, one hard coded piece from ro, needs to be parsed properly
+        String testJSON = 
+        		"{\"@context\": {\"Identifier\": \"http://purl.org/dc/terms/identifier\"},\"Identifier\": \"tag:medici@uiuc.edu,2009:col_SqW5XUYSfhZ627ghxBfaMQ\"}";
+              		    
 	        WebResource webResource = Client.create().resource(pdtSystemUrl);
 	        ClientResponse response = webResource.path("harvest")
 	                .path("publishRO")
 	                .accept("application/json")
 	                .type("application/json")
-	                .post(ClientResponse.class, ros);					
+//	                .post(ClientResponse.class, ro);			
+	                .post(ClientResponse.class, testJSON);			
 			
 	        SeadStatusTracker.addStatus(context.getProperty(Constants.RO_ID), SeadStatus.PDTStatus.END.getValue());
 			
@@ -87,31 +63,4 @@ public class UpdatePdtActivity extends AbstractWorkflowActivity {
 
 
     }
-    
-    private JSONArray getAuthors(JSONObject ro) throws JSONException {
-        JSONArray authorArray = new JSONArray();
-
-        if(ro.has("creator")) {
-            Object creators = ro.get("creator");
-            if(creators instanceof JSONArray){
-                for(int i = 0 ; i < ((JSONArray) creators).length() ; i++){
-                    Object creator = ((JSONArray) creators).get(i);
-                    if(creator instanceof String){
-                        String[] params = ((String) creator).split(":",2);
-
-                        JSONObject creatorObject = new JSONObject();
-                        creatorObject.put("@type", "Person");
-                        creatorObject.put("name", params[0]);
-                        if(params.length > 1){
-                            creatorObject.put("@id", params[1]);
-                        }
-                        creatorObject.put("email", "");
-                        authorArray.put(creatorObject);
-                    }
-                }
-            }
-        }
-
-        return authorArray;
-    }    
 }
