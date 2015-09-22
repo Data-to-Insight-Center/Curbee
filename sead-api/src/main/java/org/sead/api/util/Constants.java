388c38ee5055b4a91e78001c4a164604ec89d103
Isuru Suriarachchi <isurues@gmail.com>
2015-09-21 23:25:17 -0400

    Redirecting matchmaking calls to matchmaker service from the API

--- sead-api/src/main/java/org/sead/api/util/Constants.java
+++ sead-api/src/main/java/org/sead/api/util/Constants.java
@@ -1,47 +1,32 @@
 package org.sead.api.util;
 
-
-import org.apache.commons.io.IOUtils;
-
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
+import java.util.Properties;
 
 public class Constants {
 
     public static String pdtUrl;
     public static String curBeeUrl;
+    public static String matchmakerUrl;
 
     static {
         try {
             loadConfigurations();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void loadConfigurations() throws IOException {
-        InputStream inputStream =
-                Constants.class.getResourceAsStream("./default.properties");
-
-        StringWriter writer = new StringWriter();
-        IOUtils.copy(inputStream, writer);
-
-        String result = writer.toString();
-        String[] pairs = result.trim().split(
-                "\n|\\=");
-
-
-        for (int i = 0; i + 1 < pairs.length;) {
-            String name = pairs[i++].trim();
-            String value = pairs[i++].trim();
-
-            if(name.equals("pdt.url")){
-                pdtUrl = value;
-            }
-            if(name.equals("curBee.url")){
-                curBeeUrl = value;
-            }
+    private static void loadConfigurations() throws Exception {
+        Properties properties = new Properties();
+        InputStream inputStream = Constants.class.getResourceAsStream("default.properties");
+        if (inputStream != null) {
+            properties.load(inputStream);
+        } else {
+            throw new Exception("Error while reading Matchmaker properties");
         }
+        pdtUrl = properties.getProperty("pdt.url");
+        curBeeUrl = properties.getProperty("curBee.url");
+        matchmakerUrl = properties.getProperty("matchmaker.url");
     }
 }
