d3c50c8d9d7a37a025361c4b498a2f6743098cc5
Jim Myers <myersjd@umich.edu>
2015-10-02 18:52:44 -0400

    just fixing indents in files no code changes

--- sead-api/src/main/webapp/js/services.js
+++ sead-api/src/main/webapp/js/services.js
@@ -1,9 +1,9 @@
 var apiprefix = "./cp";
 
 function loadPeople() {
-$('#people>div>div>table>tbody').empty();
+	$('#people>div>div>table>tbody').empty();
 
-$.ajax({
+	$.ajax({
 		type : "GET",
 		url : apiprefix + "/people",
 		dataType : "json",
@@ -13,11 +13,10 @@ $.ajax({
 
 }
 
-
 function loadRepos() {
-$('#repositories>div>div>table>tbody').empty();
+	$('#repositories>div>div>table>tbody').empty();
 
-$.ajax({
+	$.ajax({
 		type : "GET",
 		url : apiprefix + "/repositories",
 		dataType : "json",
@@ -28,273 +27,359 @@ $.ajax({
 }
 
 function errorAlert(msg) {
-alert(JSON.stringify(msg));
+	alert(JSON.stringify(msg));
 }
-
 
 function updatePeople(people) {
 
+	(function($) {
 
-(function ($) {
+		$('#peoplefilter').keyup(function() {
 
-        $('#peoplefilter').keyup(function () {
+			var rex = new RegExp($(this).val(), 'i');
+			$('#people .searchable tr').hide();
+			$('#people .searchable tr').filter(function() {
+				return rex.test($(this).text());
+			}).show();
 
-            var rex = new RegExp($(this).val(), 'i');
-            $('#people .searchable tr').hide();
-            $('#people .searchable tr').filter(function () {
-                return rex.test($(this).text());
-            }).show();
+		})
 
-        })
+	}(jQuery));
 
-    }(jQuery));
+	people = people.persons;
+	for ( var x in people) {
 
-people=people.persons;
-for(var x in people) {
+		$('#people>div>div>table')
+				.append(
+						$('<tr/>')
+								.append(
+										$('<td/>')
+												.append(
+														$('<a/>')
+																.text(
+																		people[x].givenName
+																				+ people[x].familyName)
+																.attr(
+																		'href',
+																		apiprefix
+																				+ '/people/'
+																				+ people[x]['@id'])))
+								.append(
+										$('<td/>')
+												.append(
+														$('<a/>')
+																.text(
+																		people[x].email)
+																.attr(
+																		'href',
+																		"mailTo:"
+																				+ people[x].email)))
+								.append(
+										$('<td/>')
+												.append(
+														$('<a/>')
+																.text(
+																		people[x].PersonalProfileDocument)
+																.attr(
+																		'href',
+																		people[x].PersonalProfileDocument)))
+								.append($('<td/>').text(people[x].affiliation)));
+	}
 
-$('#people>div>div>table')
-.append($('<tr/>')
-	.append($('<td/>').append($('<a/>').text(people[x].givenName + people[x].familyName).attr('href',apiprefix + '/people/' + people[x]['@id'])))
-	.append($('<td/>').append($('<a/>').text(people[x].email).attr('href',"mailTo:" + people[x].email)))
-.append($('<td/>').append($('<a/>').text(people[x].PersonalProfileDocument).attr('href',people[x].PersonalProfileDocument)))
-.append($('<td/>').text(people[x].affiliation)));
 }
-
-}
-
 
 function updateRepos(repos) {
 
+	(function($) {
 
-(function ($) {
+		$('#repofilter').keyup(function() {
 
-        $('#repofilter').keyup(function () {
+			var rex = new RegExp($(this).val(), 'i');
+			$('#repositories .searchable tr').hide();
+			$('#repositories .searchable tr').filter(function() {
+				return rex.test($(this).text());
+			}).show();
 
-            var rex = new RegExp($(this).val(), 'i');
-            $('#repositories .searchable tr').hide();
-            $('#repositories .searchable tr').filter(function () {
-                return rex.test($(this).text());
-            }).show();
+		})
 
-        })
+	}(jQuery));
 
-    }(jQuery));
+	for ( var x in repos) {
 
-for(var x in repos) {
-
-var rowId=makeCSSName(repos[x].orgidentifier);
-$('#repositories>div>div>table')
-.append($('<tr/>').attr('id', rowId)
-	.append($('<td/>').append($('<a/>').text(repos[x].repositoryName).attr('href',apiprefix + '/repositories/' + repos[x].orgidentifier)))
-	.append($('<td/>').append($('<a/>').text(repos[x].repositoryURL).attr('href',repos[x].repositoryURL)))
-.append($('<td/>').text(repos[x].lastUpdate)).attr('onmouseover','showDets(\"' + apiprefix + '/repositories/\",\"' + repos[x].orgidentifier +'\", ' + '\"#repositoriesDetails\")')
-);
-}
+		var rowId = makeCSSName(repos[x].orgidentifier);
+		$('#repositories>div>div>table')
+				.append(
+						$('<tr/>')
+								.attr('id', rowId)
+								.append(
+										$('<td/>')
+												.append(
+														$('<a/>')
+																.text(
+																		repos[x].repositoryName)
+																.attr(
+																		'href',
+																		apiprefix
+																				+ '/repositories/'
+																				+ repos[x].orgidentifier)))
+								.append(
+										$('<td/>')
+												.append(
+														$('<a/>')
+																.text(
+																		repos[x].repositoryURL)
+																.attr(
+																		'href',
+																		repos[x].repositoryURL)))
+								.append($('<td/>').text(repos[x].lastUpdate))
+								.attr(
+										'onmouseover',
+										'showDets(\"' + apiprefix
+												+ '/repositories/\",\"'
+												+ repos[x].orgidentifier
+												+ '\", '
+												+ '\"#repositoriesDetails\")'));
+	}
 
 }
 
 function loadRequests(stage, desired, anchor) {
-map={};
-$(anchor + '>div>div>table>tbody').empty();
-$.ajax({
+	map = {};
+	$(anchor + '>div>div>table>tbody').empty();
+	$.ajax({
 		type : "GET",
 		url : apiprefix + "/researchobjects",
 		dataType : "json",
-		success : function (response) {
-		         updateRequests(response, stage, desired, anchor)},
+		success : function(response) {
+			updateRequests(response, stage, desired, anchor)
+		},
 		error : errorAlert
 	});
 
 }
-
 
 function updateRequests(requests, stage, desired, anchor) {
 
-(function ($) {
+	(function($) {
 
-        $(anchor + 'filter').keyup(function () {
+		$(anchor + 'filter').keyup(function() {
 
-            var rex = new RegExp($(this).val(), 'i');
-            $('#requests .searchable tr').hide();
-            $('#requests .searchable tr').filter(function () {
-                return rex.test($(this).text());
-            }).show();
+			var rex = new RegExp($(this).val(), 'i');
+			$('#requests .searchable tr').hide();
+			$('#requests .searchable tr').filter(function() {
+				return rex.test($(this).text());
+			}).show();
 
-        })
+		})
 
-    }(jQuery));
+	}(jQuery));
 
+	for ( var x in requests) {
+		if (requests[x].Aggregation != null) {
+			var statusArray = requests[x].Status;
+			statusArray.sort(function(a, b) {
+				// Turn your strings into dates, and then subtract them
+				// to get a value that is either negative, positive, or zero.
 
-for(var x in requests) {
-if(requests[x].Aggregation != null) {
-var statusArray = requests[x].Status;
-statusArray.sort(function(a,b){
-  // Turn your strings into dates, and then subtract them
-  // to get a value that is either negative, positive, or zero.
+				return new Date(a.date) - new Date(b.date);
+			});
 
-  return new Date(a.date) - new Date(b.date);
-});
+			if ((statusArray[statusArray.length - 1].stage == stage) == desired) {
 
+				var rowId = makeCSSName(requests[x].Aggregation.Identifier);
 
-if((statusArray[statusArray.length-1].stage==stage)==desired) {
+				$(anchor + '>div>div>table')
+						.append(
+								$('<tr/>')
+										.append(
+												$('<td/>')
+														.append(
+																$('<div/>')
+																		.text(
+																				requests[x].Aggregation.Title)))
+										.append(
+												$('<td/>')
+														.append(
+																$('<div/>')
+																		.text(
+																				statusArray[0].date)))
+										.append(
+												$('<td/>')
+														.append(
+																$('<div/>')
+																		.text(
+																				statusArray[statusArray.length - 1].date)))
+										.append(
+												$('<td/>')
+														.append(
+																$('<div/>')
+																		.text(
+																				requests[x].Repository)))
+										.attr(
+												'onmouseover',
+												'showDets(\"'
+														+ apiprefix
+														+ '/researchobjects/\",\"'
+														+ requests[x].Aggregation.Identifier
+														+ '\", ' + '\"'
+														+ anchor + 'Details\")')
+										.attr("id", rowId));
+			}
 
-var rowId = makeCSSName(requests[x].Aggregation.Identifier);
+		}
+	}
 
-$(anchor + '>div>div>table').append($('<tr/>').append($('<td/>')
-	.append($('<div/>').text(requests[x].Aggregation.Title))).append($('<td/>').append($('<div/>').text(statusArray[0].date)))
-.append($('<td/>').append($('<div/>').text(statusArray[statusArray.length-1].date)))
-	.append($('<td/>').append($('<div/>').text(requests[x].Repository))).attr('onmouseover','showDets(\"' + apiprefix + '/researchobjects/\",\"' + requests[x].Aggregation.Identifier +'\", ' + '\"' + anchor + 'Details\")').attr("id",rowId));
 }
-
-}
-}
-
-}
-var map={};
-var lastfunc={};
+var map = {};
+var lastfunc = {};
 
 function showDets(prefix, item, anchor) {
-var currentROFunc = $('input[name=' + anchor.substring(1) + 'funcs]:checked').val();
-if((map[anchor]==item)&&(currentROFunc==lastfunc[anchor])) {
-return;
-} else {
-if((map[anchor]!=null)&&(map[anchor]!=item)) {
-$('#' + makeCSSName(map[anchor])).removeClass('selecteditem');
+	var currentROFunc = $(
+			'input[name=' + anchor.substring(1) + 'funcs]:checked').val();
+	if ((map[anchor] == item) && (currentROFunc == lastfunc[anchor])) {
+		return;
+	} else {
+		if ((map[anchor] != null) && (map[anchor] != item)) {
+			$('#' + makeCSSName(map[anchor])).removeClass('selecteditem');
+		}
+
+		$('#' + makeCSSName(item)).addClass('selecteditem');
+
+		if (currentROFunc == "Details") {
+
+			$.ajax({
+				type : "GET",
+				url : prefix + item,
+				dataType : "json",
+				success : function(response) {
+					showRequest(response, anchor)
+				},
+				error : errorAlert
+			});
+		} else if (currentROFunc == "Status") {
+			$.ajax({
+				type : "GET",
+				url : prefix + item + '/status',
+				dataType : "json",
+				success : function(response) {
+					showRequest(response, anchor)
+				},
+				error : errorAlert
+			});
+		} else if (currentROFunc == "Matches") {
+			$.ajax({
+				type : "GET",
+				url : prefix + item,
+				dataType : "json",
+				success : function(response) {
+					$.ajax({
+						type : "POST",
+						url : prefix + 'matchingrepositories',
+						contentType : "application/json; charset=utf-8",
+						dataType : "json",
+						data : JSON.stringify(response),
+						success : function(matchresponse) {
+							showRequest(matchresponse, anchor)
+						},
+						error : errorAlert
+					});
+				},
+				error : errorAlert
+			});
+
+		}
+		map[anchor] = item;
+		lastfunc[anchor] = currentROFunc;
+	}
+
 }
-
-$('#' + makeCSSName(item)).addClass('selecteditem');
-
-if(currentROFunc=="Details") {
-
-$.ajax({
-		type : "GET",
-		url : prefix+item,
-		dataType : "json",
-		success : function (response) {
-		         showRequest(response, anchor)},
-		error : errorAlert
-	});
-}else if (currentROFunc=="Status") {
-$.ajax({
-		type : "GET",
-		url : prefix+item + '/status',
-		dataType : "json",
-		success : function (response) {
-		         showRequest(response, anchor)},
-		error : errorAlert
-	});
-} else if (currentROFunc=="Matches") {
-$.ajax({
-		type : "GET",
-		url : prefix+item ,
-		dataType : "json",
-		success : function (response) {
-$.ajax({
-		type : "POST",
-		url : prefix+ 'matchingrepositories',
-	     contentType: "application/json; charset=utf-8",
-		dataType : "json",
-		data:  JSON.stringify(response),
-		success : function (matchresponse) {
-		         showRequest(matchresponse, anchor)},
-		error : errorAlert
-	});
-},
-		error : errorAlert
-	});
-
-}
-map[anchor] = item;
-lastfunc[anchor]=currentROFunc;
-} 
-
-
-}
-
-
 
 function showRequest(item, anchor) {
 
-item = whap(item);
-if(!($(anchor + ' .jqtree-tree').length)) {
-    $(anchor).tree({
-        data: item,
-		autoOpen: 1,
-       onCreateLi: function(node, $li) {
-            // Append a link to the jqtree-element div.
-           var text = $li.find('.jqtree-title').text();
-		var i = text.indexOf(":");
-		
-		$li.find('.jqtree-title').html("<div class='key'>" + text.substring(0,i) + "</div><div class='value'>" + text.substring(i) + "</div>");
+	item = whap(item);
+	if (!($(anchor + ' .jqtree-tree').length)) {
+		$(anchor).tree(
+				{
+					data : item,
+					autoOpen : 1,
+					onCreateLi : function(node, $li) {
+						// Append a link to the jqtree-element div.
+						var text = $li.find('.jqtree-title').text();
+						var i = text.indexOf(":");
 
-       }
+						$li.find('.jqtree-title').html(
+								"<div class='key'>" + text.substring(0, i)
+										+ "</div><div class='value'>"
+										+ text.substring(i) + "</div>");
 
-});
-} else {
-    $(anchor).tree('loadData', item);
-}
+					}
+
+				});
+	} else {
+		$(anchor).tree('loadData', item);
+	}
 }
 
 function whap(rawtree) {
-//Build a tree as jqTree expects
-var tree = [];
-if(rawtree instanceof Array) {
-	for(var i in rawtree) {
-		addNode(tree, i, rawtree[i]);
-	}
-} else {
-	for(var i in rawtree) {
-	if(rawtree.hasOwnProperty(i)) {
-
-if (typeof rawtree[i] === 'object') {
-		addNode(tree, i, rawtree[i]);
+	// Build a tree as jqTree expects
+	var tree = [];
+	if (rawtree instanceof Array) {
+		for ( var i in rawtree) {
+			addNode(tree, i, rawtree[i]);
+		}
 	} else {
-	      var child = {};
-		child['label'] = i + ": " + rawtree[i];
+		for ( var i in rawtree) {
+			if (rawtree.hasOwnProperty(i)) {
 
-		tree.push(child);
+				if (typeof rawtree[i] === 'object') {
+					addNode(tree, i, rawtree[i]);
+				} else {
+					var child = {};
+					child['label'] = i + ": " + rawtree[i];
+
+					tree.push(child);
+				}
+
+			}
+		}
 	}
-
-} 
-}
-}
-return tree;
+	return tree;
 }
 
 function addNode(tree, label, rawobject) {
-  var node = {};
-	node['label']= label;
+	var node = {};
+	node['label'] = label;
 
 	var children = [];
-var prefix="";
-if(rawobject instanceof Array) {
-prefix=label + "_";
-}
-	for(var i in rawobject) {
-	if(rawobject.hasOwnProperty(i)) {
-	if (typeof rawobject[i] === 'object') {
-		addNode(children, prefix+i, rawobject[i]);
-	} else {
-
-	      var child = {};
-		child['label'] = prefix+i + ": " + rawobject[i];
-		children.push(child);
+	var prefix = "";
+	if (rawobject instanceof Array) {
+		prefix = label + "_";
 	}
+	for ( var i in rawobject) {
+		if (rawobject.hasOwnProperty(i)) {
+			if (typeof rawobject[i] === 'object') {
+				addNode(children, prefix + i, rawobject[i]);
+			} else {
 
-}
+				var child = {};
+				child['label'] = prefix + i + ": " + rawobject[i];
+				children.push(child);
+			}
 
-}
-if(children.length>0) {
-node['children']=children;
-}
+		}
+
+	}
+	if (children.length > 0) {
+		node['children'] = children;
+	}
 	tree.push(node);
 }
 
 function makeCSSName(id) {
-return id.replace(/[~!@\$%\^&\*\(\)\+=,\.\/';:"\?><\[\]\\\{}\|`#]/g, "_");
-} 
+	return id.replace(/[~!@\$%\^&\*\(\)\+=,\.\/';:"\?><\[\]\\\{}\|`#]/g, "_");
+}
 
 function check(rofuncs) {
 
-$('.searchable .selecteditem').trigger('mouseover');
-} 
+	$('.searchable .selecteditem').trigger('mouseover');
+}
